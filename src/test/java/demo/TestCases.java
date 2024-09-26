a169ac71aa3b0ded602cb9073dec5be459ace005
Lakesh Sahu <lakeshsahu22@gmail.com>
2024-09-26 17:49:09 +0530

    Improved Readability

--- src/test/java/demo/TestCases.java
+++ src/test/java/demo/TestCases.java
@@ -13,133 +13,246 @@ import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 import java.time.Duration;
-
 import java.util.logging.Level;
-// import io.github.bonigarcia.wdm.WebDriverManager;
 import demo.wrappers.Wrappers;
 
 public class TestCases {
+        // Creating instance variable of ChromeDriver class
         ChromeDriver driver;
+        // Creating instance variable of WebDriverWait class
         WebDriverWait wait;
-        Wrappers wp;
-        /*
-         * TO DO: Write your tests here with testng @Test annotation.
-         * Follow `testCase01` `testCase02`... format or what is provided in
-         * instructions
-         */
+        // Creating instance variable of Wrappers class
+        Wrappers wp; 
 
-         // Opening url
+        // Verify url is opening
         @Test
         public void testCase01() throws InterruptedException {
-                System.out.println("Start Tese case: testCase01");
-                wp.openUrl(
-                                "https://docs.google.com/forms/d/e/1FAIpQLSep9LTMntH5YqIXa5nkiPKSs283kdwitBBhXWyZdAS-e4CxBQ/viewform");
-                System.out.println("URL Opened");
-                System.out.println("end Test case: testCase01");
+                try {
+                        System.out.println("Start Test case: testCase01");
+
+                        // Opening the required url
+                        wp.openUrl(
+                                        "https://docs.google.com/forms/d/e/1FAIpQLSep9LTMntH5YqIXa5nkiPKSs283kdwitBBhXWyZdAS-e4CxBQ/viewform");
+
+                        System.out.println("testCase01: Passed");
+
+                } catch (Exception e) {
+                        System.out.println("testCase01 Failed");
+                        
+                        //Printing the exception details
+                        e.printStackTrace();
+                }
+
+                System.out.println("End Test case: testCase01");
         }
 
-         // Entering Name
+        // Verify user is able to enter name in the Name textbox
         @Test
         public void testCase02() throws InterruptedException {
-                System.out.println("Start Tese case: testCase02");
-                WebElement nameTxtBox = wait.until(ExpectedConditions.visibilityOfElementLocated(
-                                By.xpath("//span[text()='Name']/ancestor::div[3]/following-sibling::div//input")));
-                Thread.sleep(1000);
-                wp.sendKeys(nameTxtBox, "Crio Learner");
-                System.out.println("Crio Learner is entered in the input box");
-                System.out.println("end Test case: testCase02");
+                try {
+                        System.out.println("Start Test case: testCase02");
+
+                        //Locating textbox having "Name" text
+                        WebElement nameTxtBox = wait.until(ExpectedConditions.visibilityOfElementLocated(
+                                        By.xpath("//span[text()='Name']/ancestor::div[3]/following-sibling::div//input")));
+                        
+                        Thread.sleep(1000);
+
+                        //Entering Name
+                        wp.sendKeys(nameTxtBox, "Crio Learner");
+
+                        System.out.println("testCase02: Passed");
+                } catch (Exception e) {
+                        System.out.println("testCase02 Failed");
+
+                        //Printing the exception details
+                        e.printStackTrace();
+                }
+                System.out.println("End Test case: testCase02");
         }
 
-        // Entering Why are you practicing Automation? answer
+        // Verify user is able to enter details and epoch time in "Why are you practicing Automation?" textbox
         @Test
         public void testCase03() throws InterruptedException {
-                System.out.println("Start Tese case: testCase03");
-                WebElement practAuto = driver.findElement(By.xpath(
-                                "//span[text()='Why are you practicing Automation?']/ancestor::div[3]/following-sibling::div//textarea"));
-                long epochTime = wp.epochTime();
-                String text = "I want to be the best QA Engineer! " + epochTime;
-                wp.sendKeys(practAuto, text);
-                System.out.println("epoch time : " + epochTime);
-                System.out.println("end Test case: testCase03");
+                try {
+                        System.out.println("Start Test case: testCase03");
+
+                        //Locating textbox having "Why are you practicing Automation?" text
+                        WebElement practAuto = driver.findElement(By.xpath(
+                                        "//span[text()='Why are you practicing Automation?']/ancestor::div[3]/following-sibling::div//textarea"));
+                        
+                        //Storing epoch time
+                        long epochTime = wp.epochTime();
+
+                        //Storing the text to be enter + epoch time
+                        String text = "I want to be the best QA Engineer! " + epochTime;
+
+                        //Entering the text to the textbox
+                        wp.sendKeys(practAuto, text);
+                        System.out.println("epoch time : " + epochTime);
+
+                        System.out.println("testCase03: Passed");
+                } catch (Exception e) {
+                        System.out.println("testCase03 Failed");
+                        
+                        //Printing the exception details
+                        e.printStackTrace();
+                }
+                System.out.println("End Test case: testCase03");
         }
 
-        // Selecting radio button How much experience do you have in Automation Testing?
+        // Selecting radio button for "How much experience do you have in Automation Testing?""
         @Test
         public void testCase04() throws InterruptedException {
-                System.out.println("Start Tese case: testCase04");
-                WebElement expRadioBtn = driver.findElement(By.xpath(
-                                "//span[text()='How much experience do you have in Automation Testing?']/ancestor::div[3]/following-sibling::div//span[text()='0 - 2']/ancestor::div[2]/preceding-sibling::div"));
-                wp.click(expRadioBtn);
-               System.out.println("0 - 2 experieance selected");
-               System.out.println("end Test case: testCase04");
+                try {
+                        System.out.println("Start Test case: testCase04");
+
+                        //Storing experience between options: 0 - 2, 3 - 5, 6 - 10, > 10
+                        String experience = "0 - 2";
+
+                        //Selecting radio button for above experience
+                        wp.experieanceRadioBtn(experience);
+
+                        System.out.println("testCase04: Passed");
+                } catch (Exception e) {
+                        System.out.println("testCase04 Failed");
+
+                        //Printing the exception details
+                        e.printStackTrace();
+                }
+                System.out.println("End Test case: testCase04");
         }
 
-        //Selecting checkbox Which of the following have you learned in Crio.Do for Automation Testing?
+        // Selecting checkbox for "Which of the following have you learned in Crio.Do for Automation Testing?"
         @Test
         public void testCase05() throws InterruptedException {
-                System.out.println("Start Tese case: testCase05");
-                wp.checkBoxSelect("Java");
-                wp.checkBoxSelect("Selenium");
-                wp.checkBoxSelect("TestNG");
-                System.out.println("Java, Selenium and TestNG is selected");
-                System.out.println("end Test case: testCase05");
+                try {
+                        System.out.println("Start Test case: testCase05");
+
+                        //Storing options to be selected
+                        String[] optionsToSelect = {"Java", "Selenium", "TestNG"};
+
+                        for(String option : optionsToSelect) {
+                                //Selecting checkbox for "Java"
+                                wp.checkBoxSelect(option);
+
+                                System.out.println(option + " is selected");
+                        }
+                        
+                        System.out.println("testCase05: Passed");
+                } catch (Exception e) {
+                        System.out.println("testCase05 Failed");
+
+                        //Printing the exception details
+                        e.printStackTrace();
+                }
+                System.out.println("End Test case: testCase05");
         }
-        
-        //Selecting How should you be addressed?
+
+        // Selecting How should you be addressed?
         @Test
         public void testCase06() throws InterruptedException {
-                System.out.println("Start Tese case: testCase06");
-                Thread.sleep(1500);
-                WebElement dropDown = driver.findElement(
-                                By.xpath("//span[text()='How should you be addressed?']/ancestor::div[4]//span[text()='Choose']"));
-                wp.click(dropDown);
-                wp.dropDownOptionClick("Mr");
-                System.out.println("Mr selected");
-                System.out.println("end Test case: testCase06");
+                try {
+                        System.out.println("Start Test case: testCase06");
+
+                        //Locating the dropdown button
+                        WebElement dropDown = driver.findElement(
+                                        By.xpath("//span[text()='How should you be addressed?']/ancestor::div[4]//span[text()='Choose']"));
+
+                        //Clicking on the dropdown
+                        wp.click(dropDown);
+
+                        //Storing the option to select
+                        String optionToSelect = "Mr";
+
+                        //Selecting the option
+                        wp.dropDownOptionClick(optionToSelect);
+
+                        System.out.println(optionToSelect + " selected");
+                        System.out.println("testCase06: Passed");
+
+                } catch (Exception e) {
+                        System.out.println("testCase06 Failed");
+
+                        //Printing the exception details
+                        e.printStackTrace();
+                }
+                System.out.println("End Test case: testCase06");
         }
 
-        // Entering date before 7 days form today
+        // Entering date before 7 days from today
         @Test
         public void testCase07() throws InterruptedException {
-                System.out.println("Start Tese case: testCase07");
-                wp.day7Before(7);
-                System.out.println("end Test case: testCase07");
+                try {
+                        System.out.println("Start Test case: testCase07");
+
+                        //Selecting the date before today
+                        wp.day7Before(7);
+
+                        System.out.println("testCase07: Passed");
+                } catch (Exception e) {
+                        System.out.println("testCase07 Failed");
+
+                        //Printing the exception details
+                        e.printStackTrace();
+                }
+                System.out.println("End Test case: testCase07");
         }
-         // Entering current time and clicking submit button
+
+        // Entering time and clicking submit button
         @Test
         public void testCase08() throws InterruptedException {
-                System.out.println("Start Tese case: testCase08");
-                wp.time("07:30");
-                
-                WebElement submitBtn = driver.findElement(By.xpath("//span[text()='Submit']"));
-                wp.click(submitBtn);
-                System.out.println("07:30 Entered");
-                System.out.println("end Test case: testCase08");
+                try {
+                        System.out.println("Start Test case: testCase08");
+
+                        //Entering the time in "HH:mm" formate
+                        wp.time("07:30");
+
+                        //Locating the submit button
+                        WebElement submitBtn = driver.findElement(By.xpath("//span[text()='Submit']"));
+
+                        //Clicking on the submit button
+                        wp.click(submitBtn);
+
+                        System.out.println("testCase08: Passed");
+                } catch (Exception e) {
+                        System.out.println("testCase08 Failed");
+
+                        //Printing the exception details
+                        e.printStackTrace();
+                }
+                System.out.println("End Test case: testCase08");
         }
 
-        //Checking successful message and printing
+        // Checking successful message and printing
         @Test
         public void testCase09() throws InterruptedException {
-                System.out.println("Start Tese case: testCase09");
-                WebElement result = wait.until(ExpectedConditions
-                                .visibilityOfElementLocated(By.xpath(
-                                                "//div[text()='Thanks for your response, Automation Wizard!']")));
-                System.out.println(result.getText());
-                System.out.println("end Test case: testCase09");
+                try {
+                        System.out.println("Start Test case: testCase09");
+
+                        //Waiting and locating for the successful WebElement
+                        WebElement result = wait.until(ExpectedConditions
+                                        .visibilityOfElementLocated(By.xpath(
+                                                        "//div[text()='Thanks for your response, Automation Wizard!']")));
+                        
+                        //Printing the successful message
+                        System.out.println(result.getText());
+
+                        System.out.println("testCase09: Passed");
+                } catch (Exception e) {
+                        System.out.println("testCase09 Failed");
+
+                        //Printing the exception details
+                        e.printStackTrace();
+                }
+                System.out.println("End Test case: testCase09");
         }
 
-
-        /*
-         * Do not change the provided methods unless necessary, they will help in
-         * automation and assessment
-         */
         @BeforeTest
         public void startBrowser() {
                 System.setProperty("java.util.logging.config.file", "logging.properties");
 
-                // NOT NEEDED FOR SELENIUM MANAGER
-                // WebDriverManager.chromedriver().timeout(30).setup();
-
+                //Creating the ChromeOptions class object
                 ChromeOptions options = new ChromeOptions();
                 LoggingPreferences logs = new LoggingPreferences();
 
@@ -150,17 +263,24 @@ public class TestCases {
 
                 System.setProperty(ChromeDriverService.CHROME_DRIVER_LOG_PROPERTY, "build/chromedriver.log");
 
+                //Initializing the ChromeDriver object
                 driver = new ChromeDriver(options);
+
+                //Initializing the WebDriverWait object
                 wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+
+                //Maximizing the browser window
                 driver.manage().window().maximize();
 
+                //Initializing the Wrappers class object
                 wp = new Wrappers(driver, wait);
+
                 System.out.println("starting browser");
         }
 
+        //Quitting the browser window
         @AfterTest
         public void endTest() {
-                // driver.close();
                 driver.quit();
         }
 }
--- src/test/java/demo/wrappers/Wrappers.java
+++ src/test/java/demo/wrappers/Wrappers.java
@@ -1,111 +1,168 @@
 package demo.wrappers;
 
-// import java.time.Instant;
-// import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.LocalDate;
 import java.time.LocalTime;
-// import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
-// import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-// import java.time.Duration;
 
 public class Wrappers {
-    /*
-     * Write your selenium wrappers here
-     */
+    //Creating ChromeDriver instance variable
     ChromeDriver driver;
-    WebDriverWait wait;
 
+    //Creating WebDriverWait instance variable
+    WebDriverWait wait; 
+
+    //Constructor of Wrappers class
     public Wrappers(ChromeDriver driver, WebDriverWait wait) {
         System.out.println("Creating wrapper constructor");
+
+        //Initializing the class ChromeDriver instance variable
         this.driver = driver;
+
+        //Initializing the class WebDriverWait instance variable
         this.wait = wait;
     }
 
+    //To click an WebElement
     public void click(WebElement element) {
-        System.out.println("Click method called");
         element.click();
     }
 
-    public void sendKeys(WebElement element, String message) {
-        System.out.println("sendKeys method called");
+    //To enter text in a WebElement
+    public void sendKeys(WebElement element, String text) {
+        //Clearing the WebElement
         element.clear();
-        element.sendKeys(message);
+
+        //Sending text to the WebElement
+        element.sendKeys(text);
     }
 
+    //To open url
     public void openUrl(String url) {
         System.out.println("open url called");
+
+        //Opening url
         driver.get(url);
     }
 
+    //To return epoch time
     public long epochTime() {
-        System.out.println("epochTime called");
+        //Storing epoch time
         long epochTime = System.currentTimeMillis();
+
+        //returning epoch time
         return epochTime;
     }
 
+    //Selecting radio button for given experience
+    public void experieanceRadioBtn(String exp) {
+        //Locating Radio Button for given experience 
+        WebElement expRadioBtn = driver.findElement(By.xpath(
+            "//span[text()='How much experience do you have in Automation Testing?']/ancestor::div[3]/following-sibling::div//span[text()='" + exp + "']/ancestor::div[2]/preceding-sibling::div"));
+
+        //Clicking on radio button                 
+        click(expRadioBtn);
+
+        System.out.println(exp + " experieance selected");
+    }
+
+    //Selecting Checkbox for given option text
     public void checkBoxSelect(String option) {
-        System.out.println("checkBoxSelect called");
+        //Locating the checkbox for given option text
         WebElement optionChkBox = driver.findElement(By.xpath(
                 "//span[text()='Which of the following have you learned in Crio.Do for Automation Testing?']/ancestor::div[3]/following-sibling::div//span[text()='"
                         + option + "']/ancestor::div[2]/preceding-sibling::div"));
+
+        //Selecting the checkbox
         optionChkBox.click();
     }
 
+    //To select option from the dropdown of "How should you be addressed"
     public void dropDownOptionClick(String option) throws InterruptedException {
-        System.out.println("dropDownOptionClick called");
+
+        //Waiting for the dropdown to open
         Thread.sleep(1500);
+
+        //Locating the dropdown option with given option 
         WebElement optionSelect = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(
                 "(//span[text()='How should you be addressed?']/ancestor::div[4]//span[text()='" + option + "'])[2]")));
+
+        //Clicking on the dropdown option
         optionSelect.click();
     }
 
-    public void day7Before(int minusDay) {
-        System.out.println("day7Before called");
+    //Selecting the date before today
+    public void day7Before(int minusDay) throws InterruptedException {
+
+        //Getting todays date in the "yyy-MM-dd" pattern
         LocalDate currentDate = LocalDate.now();
+
+        //Getting given number of days before todays date
         LocalDate newDate = currentDate.minusDays(minusDay);
-        // String dateWithoutHyphen = String.valueOf(newDate).replace("-", "");
-        // WebElement input = driver
-        // .findElement(By.xpath("//span[text()='What was the date 7 days
-        // ago?']/ancestor::div[4]//input"));
-        // input.sendKeys(dateWithoutHyphen.substring(6));
-        // input.sendKeys(dateWithoutHyphen.substring(4, 6));
-        // input.sendKeys(dateWithoutHyphen.substring(0, 4));
+
+        //Formate the stored date into "dd-MM-yyyy" pattern
         DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+
+        //Converting formated date into String
         String date = newDate.format(dtf);
+
+        //Locating the input box
         WebElement input = driver
                 .findElement(By.xpath("//span[text()='What was the date 7 days ago?']/ancestor::div[4]//input"));
-        input.sendKeys(String.valueOf(date));
+
+        //Entering date to the inputbox 
+        sendKeys(input, String.valueOf(date));
     }
 
-    public void time(String timeToEnter) {
-        System.out.println("time called");
-        // LocalTime currentTime = LocalTime.now();
-        // DateTimeFormatter dtf = DateTimeFormatter.ofPattern("HH");
-        // String hr = currentTime.format(dtf);
-        // DateTimeFormatter dtf2 = DateTimeFormatter.ofPattern("mm");
-        // String mm = currentTime.format(dtf2);
-        // WebElement hourInput = driver.findElement(By.xpath("(//span[text()='What is
-        // the time right now?']/ancestor::div[4]//input)[1]"));
-        // // hourInput.sendKeys(time.substring(0, 2));
-        // hourInput.sendKeys(hr);
-
-        // WebElement minInput = driver.findElement(By.xpath("(//span[text()='What is
-        // the time right now?']/ancestor::div[4]//input)[2]"));
-        // // minInput.sendKeys(time.substring(3, 5));
-        // minInput.sendKeys(mm);
-
+    //Entering the given time
+    public void time(String timeToEnter) {        
+        //Locating the hour inputbox
         WebElement hourInput = driver
                 .findElement(By.xpath("(//span[text()='What is the time right now?']/ancestor::div[4]//input)[1]"));
+
+        //Entering the hour part
         hourInput.sendKeys(timeToEnter.substring(0, 2));
 
+        //Locating the minute inputbox
         WebElement minInput = driver
                 .findElement(By.xpath("(//span[text()='What is the time right now?']/ancestor::div[4]//input)[2]"));
+        
+        //Entering the minutes part
         minInput.sendKeys(timeToEnter.substring(3, 5));
     }
-}
+
+    //Entering the current time
+    public void currentTime() {
+
+        //Getting the current time
+        LocalTime currentTime = LocalTime.now();
+
+        //Formatting the currentTime in "HH" pattern
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("HH");
+
+        //Converting formatted hour into String
+        String hr = currentTime.format(dtf);
+
+        //Formatting the currentTime in "mm" pattern
+        DateTimeFormatter dtf2 = DateTimeFormatter.ofPattern("mm");
+
+        //Converting formated minute into String
+        String mm = currentTime.format(dtf2);
+
+        //Locating hour inputbox
+        WebElement hourInput = driver.findElement(By.xpath("(//span[text()='What is the time right now?']/ancestor::div[4]//input)[1]"));
+
+        //Entering hour part in the hour inputbox
+        hourInput.sendKeys(hr);
+
+        //Locating the minute inputbox
+        WebElement minInput = driver.findElement(By.xpath("(//span[text()='What is the time right now?']/ancestor::div[4]//input)[2]"));
+
+        //Entering the minute part in the minute inputbox
+        minInput.sendKeys(mm);
+    }
+} 
