d73c9c9103195bf8dbc15a2fac0846caeb151ce2
Lakesh Sahu <lakeshsahu22@gmail.com>
2025-02-07 11:41:18 +0530

    removing extra files, updating gradle version and build.gradle file

--- run_platform_assesment.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-python assesment\assess.py build\reports\checkstyle\test.xml build\chromedriver.log build\reports\tests\test\testng-results.xml assesment\INSTRUCTIONS.json
--- run_platform_assesment.sh
+++
@@ -1,1 +0,0 @@
-python assesment/assess.py build/reports/checkstyle/test.xml build/chromedriver.log build/reports/tests/test/testng-results.xml assesment/INSTRUCTIONS.json
